270c8ded4c0572ba401f7a2a96e632399103d5f1
unknown <sid@getpassionfruit.com>
2026-04-12 01:55:43 +0530

    10gb limit

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ if (!isProduction) {
   app.use(cors());
 }
 
-app.use(express.json({ limit: '500mb' }));
-app.use(express.urlencoded({ extended: true, limit: '500mb' }));
-app.use(express.raw({ type: ['image/*', 'video/*', 'application/octet-stream'], limit: '500mb' }));
+app.use(express.json({ limit: '10gb' }));
+app.use(express.urlencoded({ extended: true, limit: '10gb' }));
+app.use(express.raw({ type: ['image/*', 'video/*', 'application/octet-stream'], limit: '10gb' }));
 
 app.use('/api', routes);
 
