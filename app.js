af1fe15ece0526e6a99e98a64ad5486e184d3247
unknown <sid@getpassionfruit.com>
2026-04-12 01:25:27 +0530

    hosted BE pt2

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ import routes from './routes/index.js';
 
 const app = express();
 
-app.use(cors());
+const isProduction = process.env.NODE_ENV === 'production';
+if (!isProduction) {
+  app.use(cors());
+}
+
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ extended: true, limit: '500mb' }));
 app.use(express.raw({ type: ['image/*', 'video/*', 'application/octet-stream'], limit: '500mb' }));
